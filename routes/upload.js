22a580ce13d2ddf9369053b7a93aeab14497041b
Shiva Aleti <aletishiva218@gmail.com>
2024-06-23 02:52:00 +0530

    added automatically delete file after getting response

--- routes/upload.js
+++ routes/upload.js
@@ -9,9 +9,10 @@ const uploadFolder = path.join(_dirname,"uploads");
 fs.mkdirSync(uploadFolder,{recursive:true})
 
 const upload = async (req,res)=>{
-    const imageUrl = `${uploadFolder}\\${req.file.path.split("\\")[1]}`;
+    const imageUrl = `${uploadFolder}/${req.file.path.split("/")[1]}`;
     try{
         const data = await generateData(imageUrl,req.file.mimetype);
+       await fs.unlinkSync(imageUrl);
     return res.json(data);
     }catch(err){
         return res.json({error:err})
